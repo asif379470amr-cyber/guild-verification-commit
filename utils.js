55c799f5aefb2953df890b8611749e53f166f167
asif379470amr-cyber <asif379470amr@gmail.com>
2026-03-18 15:10:00 +0600

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-18T09:09:53.386Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-18T09:10:04.338Z
+Commit: Fix race condition in async operations
